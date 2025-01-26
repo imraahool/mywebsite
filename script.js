b359f093271dae0b0ee6819c6a6e3ceb737cf7df
imraahool <imraahool@yahoo.com>
2025-01-26 22:24:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Initialize Supabase client with your project details
+import { createClient } from '@supabase/supabase-js';
+
 const supabaseUrl = "https://vedhqgkrvoxqrwnhbxxs.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZGhxZ2tydm94cXJ3bmhieHhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4MTMwMTcsImV4cCI6MjA1MzM4OTAxN30.uNERdYNpO9Z6vBef7XHpL8VwYk7fY46jGzTpB3u1xbw";
