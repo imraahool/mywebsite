85038a413ebf50bf0bcef045f9c2736ac85eb99e
imraahool <imraahool@yahoo.com>
2025-01-26 21:55:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Initialize Supabase client
 
-//import { createClient } from '@supabase/supabase-js'
+
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vedhqgkrvoxqrwnhbxxs.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZGhxZ2tydm94cXJ3bmhieHhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4MTMwMTcsImV4cCI6MjA1MzM4OTAxN30.uNERdYNpO9Z6vBef7XHpL8VwYk7fY46jGzTpB3u1xbw"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlZGhxZ2tydm94cXJ3bmhieHhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc4MTMwMTcsImV4cCI6MjA1MzM4OTAxN30.uNERdYNpO9Z6vBef7XHpL8VwYk7fY46jGzTpB3u1xbw";
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const uploadForm = document.getElementById("uploadForm");
